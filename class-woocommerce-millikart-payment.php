2d036c46aa4713d8a81a49f03e46e773c74e4bfe
hmammadov <huseynmammadov@yandex.ru>
2017-06-05 20:54:53 +0400

    Upload main files

---
+++ class-woocommerce-millikart-payment.php
@@ -0,0 +1,119 @@
+<?php 
+class WC_Millikart_Payment extends WC_Payment_Gateway{
+    
+	public function __construct(){
+		$this->id = 'millikart_payment';
+		$this->method_title = 'MilliKart';
+		$this->title = 'MilliKart';
+		$this->has_fields = false;
+		$this->init_form_fields();
+		$this->init_settings();
+		$this->enabled = $this->get_option('enabled');
+		$this->title = $this->get_option('title');
+        $this->mid = $this->get_option('mid');
+		$this->pskey = $this->get_option('pskey');
+		$this->currency = $this->get_option('currency');
+		$this->paymenturl = $this->get_option('paymenturl');
+		$this->payment_status = $this->get_option('payment_status');
+		add_action('woocommerce_update_options_payment_gateways_'.$this->id, array($this, 'process_admin_options'));
+	}
+    
+	public function init_form_fields(){
+        $this->form_fields = array(
+            'enabled' => array(
+                'title' 		=> __('Enable/Disable', 'woocommerce-millikart-payment'),
+                'type' 			=> 'checkbox',
+                'label' 		=> __('Enable MilliKart Payment', 'woocommerce-millikart-payment'),
+                'default' 		=> 'no',
+            ),
+            'title' => array(
+                'title' 		=> __('Payment Method Title', 'woocommerce-millikart-payment'),
+                'type' 			=> 'text',
+                'description' 	=> __('It will be shown on checkout page', 'woocommerce-millikart-payment'),
+                'default'		=> 'Visa/MasterCard',
+                'desc_tip'		=> true,
+            ),
+            'mid' => array(
+                'title' 		=> 'Merchant ID',
+                'type' 			=> 'text',
+                'description' 	=> __('MID value is given by MilliKart', 'woocommerce-millikart-payment'),
+                'placeholder'	=> __('Provided by MilliKart', 'woocommerce-millikart-payment'),
+                'desc_tip'		=> true,
+            ),
+            'pskey' => array(
+                'title' 		=> 'Key',
+                'type' 			=> 'text',
+                'description' 	=> __('Key value is given by MilliKart', 'woocommerce-millikart-payment'),
+                'placeholder'	=> __('Provided by MilliKart', 'woocommerce-millikart-payment'),
+                'desc_tip'		=> true,
+            ),
+            'currency' => array(
+                'title' 		=> __('Currency', 'woocommerce-millikart-payment'),
+                'type' 			=> 'text',
+                'description' 	=> __('Currency code of payment by ISO 4217 (ex.: AZN = 944)', 'woocommerce-millikart-payment'),
+                'placeholder'	=> __('ex.: 944', 'woocommerce-millikart-payment'),
+                'desc_tip'		=> true,
+            ),
+            'paymenturl' => array(
+                'title' 		=> __('Payment URL', 'woocommerce-millikart-payment'),
+                'type' 			=> 'text',
+                'description' 	=> __('URL to MilliKart payment page (provided by MilliKart)', 'woocommerce-millikart-payment'),
+                'placeholder'	=> __('ex.: http://.../gateway/payment/register', 'woocommerce-millikart-payment'),
+                'desc_tip'		=> true,
+            ),
+            'payment_status' => array(
+                'title' 		=> __('Payment Status', 'woocommerce-millikart-payment'),
+                'type' 			=> 'text',
+                'description' 	=> __('Payment Status checking URL (provided by MilliKart)', 'woocommerce-millikart-payment'),
+                'placeholder'	=> __('ex.: http://.../gateway/payment/status', 'woocommerce-millikart-payment'),
+                'desc_tip'		=> true,
+            )
+        );
+	}
+    
+	public function process_payment($order_id){
+		global $woocommerce, $wpdb;
+		$order = new WC_Order($order_id);
+        $millikart_payment = $wpdb->prefix . "woocommerce_millikart";
+        //randomKey function for generating reference
+        function randomKey($length = 41){
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for($i = 0; $i < $length; $i++){
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            $randomString = md5(strtoupper($randomString));
+            return strtoupper($randomString);
+        }
+        //Get required variables
+        $mid = $this->mid;
+        $currency = $this->currency;
+        $description = 'order'.$order->id;
+        $key = $this->pskey;
+        $millikartURL = $this->paymenturl;
+        $reference = randomKey();
+        $amount = $order->get_total() * 100;
+        $language = get_locale();
+        if($language == 'ru_RU'){
+            $language = 'ru';
+        }elseif($language == 'az'){
+            $language = 'az';
+        }else{
+            $language = 'en';
+        }
+        $signature = strtoupper(md5(strlen($mid).$mid.strlen($amount).$amount.strlen($currency).$currency.(!empty($description)?strlen($description).$description:"0").strlen($reference).$reference.strlen($language).$language.$key));
+        $paymentURL = $millikartURL.'?mid='.$mid.'&amount='.$amount.'&currency='.$currency.'&description='.$description.'&reference='.$reference.'&language='.$language.'&signature='.$signature.'&redirect=1';
+		//Order marks as pending
+		$order->update_status('pending', __('<b>Pending payment!</b>', 'woocommerce-millikart-payment'));
+        //Adding order reference into db
+        $reference_db_add = $wpdb->insert($millikart_payment, array('order_id' => $order->id, 'reference' => $reference, 'language' => $language));
+        // Remove cart
+		$woocommerce->cart->empty_cart();
+		// Redirect to Millikart payment page
+		return array(
+			'result'     => 'success',
+			'redirect'   => $paymentURL
+		);
+	}
+}
---
+++ woocommerce-millikart-payment.php
@@ -0,0 +1,124 @@
+<?php
+/*
+Plugin Name: WooCommerce MilliKart Payment
+Plugin URI:  
+Description: MilliKart Payment Gateway for WooCommerce.
+Version:     1.0.0
+Author:      Huseyn Mammadov
+Author URI:  https://hmammadov.ru
+License:     GPL v3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: woocommerce-millikart-payment
+Domain Path: /languages
+*/
+
+//Exit if accessed directly.
+if(!defined('ABSPATH')) exit;
+
+$active_plugins = apply_filters('active_plugins', get_option('active_plugins'));
+if(in_array('woocommerce/woocommerce.php', $active_plugins)){
+    add_filter('woocommerce_payment_gateways', 'add_millikart_payment');
+    function add_millikart_payment($gateways){
+        $gateways[] = 'WC_Millikart_Payment';
+        return $gateways; 
+    }
+    add_action('plugins_loaded', 'init_millikart_payment');
+    function init_millikart_payment(){
+        require 'class-woocommerce-millikart-payment.php';
+    }
+}
+
+//Payment status checking
+add_action('wp', function(){
+    if(isset($_GET['millikart_reference'])){
+        global $wpdb;
+        //Get order_id if reference isset
+        $order_id = $wpdb->get_results($wpdb->prepare( "SELECT order_id, language FROM " . $wpdb->prefix . "woocommerce_millikart WHERE reference = '%s' LIMIT 1", $_GET['millikart_reference']));
+        //If reference isset
+        if($order_id == true){
+	        $array = json_decode(json_encode($order_id), True);
+            $language = $array[0]['language'];
+            $order_id = $array[0]['order_id'];
+            //Get mid
+            $millikart_options = new WC_Millikart_Payment();
+            $mid = $millikart_options->mid;
+            $payment_status = $millikart_options->payment_status;
+            //Get payment status from Millikart
+            $url = $payment_status.'?mid='.$mid.'&reference='.$_GET['millikart_reference'];
+            $xml = file_get_contents($url);
+            $xml = simplexml_load_string($xml);
+            $RCcode = $xml->RC;
+            //Change payment status
+            $order = new WC_Order($order_id);
+            if($RCcode == '000'){
+                $order->update_status('processing', __('<b>Successful payment!</b>', 'woocommerce-millikart-payment'));
+            }else{
+                $order->update_status('failed', __('<b>Payment failed!</b> Get more information from <a target="_blank" href="https://mwp.millikart.az/login.php">MilliKart Account</a> or contact to your operator in MilliKart', 'woocommerce-millikart-payment'));
+            }
+            //Get checkout ID
+            $default_checkout_id = wc_get_page_id('checkout');
+            //Check if WPML or Polylang installed
+            if (function_exists('icl_object_id')){
+                //If checkout in other lang, get it ID
+                $lang_post_id = icl_object_id( $default_checkout_id , 'page', true, $language );
+            }else{
+                $lang_post_id = $default_checkout_id;
+            }
+            //Get order received URL
+            $order_received_url = wc_get_endpoint_url('order-received', $order->id, get_permalink($lang_post_id));
+            $order_received_url = add_query_arg('key', $order->order_key, $order_received_url);
+            header("Location: ".$order_received_url);
+        }else{
+            header("Location: ".get_option('home'));
+        }
+    }
+});
+
+//Creating file and tabel for Millikart
+global $millikart_db_version;
+$millikart_db_version = "1.0";
+function millikart_install(){
+    global $wpdb;
+    global $millikart_db_version;
+    //Creat table in mysql for references
+    $table_name = $wpdb->prefix . "woocommerce_millikart";
+    if($wpdb->get_var("show tables like '$table_name'") != $table_name){
+        $sql = "CREATE TABLE " . $table_name . " (
+            id_reference mediumint(9) UNSIGNED NOT NULL AUTO_INCREMENT,
+            order_id mediumint(9) UNSIGNED NOT NULL,
+            reference VARCHAR(55) NOT NULL,
+            language VARCHAR(3) NOT NULL,
+            PRIMARY KEY  (id_reference)
+        );";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        add_option("millikart_db_version", $millikart_db_version);
+    }
+    //Creating callback.php file for MilliKart reference
+    if(file_exists('../millikart_callback.php')){}else{
+        $callback = fopen('../millikart_callback.php', 'w');
+        $phpcode = "<?php\n";
+        $phpcode .= "\$reference = \$_GET['reference'];\n";
+        $phpcode .= "header(\"Location: index.php?millikart_reference=\".\$reference);\n";
+        $phpcode .= "exit;\n";
+        $phpcode .= "?>";
+        fwrite($callback, $phpcode);
+        fclose($callback);
+    }
+}
+register_activation_hook(__FILE__,'millikart_install');
+
+//On plugin deactivating
+function millikart_deactivating(){
+    //Delete millikart_callback.php
+    unlink('../callback.php');
+}    
+register_deactivation_hook(__FILE__, 'millikart_deactivating');
+
+// Add Settings link
+function millikart_settings_link($links) {
+  $settings_link = '<a href="'.admin_url( 'admin.php?page=wc-settings&tab=checkout&section=millikart_payment').'">'.__('Settings', 'woocommerce-millikart-payment').'</a>';
+  array_unshift($links, $settings_link);
+  return $links;
+}
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'millikart_settings_link');
